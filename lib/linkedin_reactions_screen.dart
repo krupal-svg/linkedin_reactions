d95aced7555a9ad056590dcc7acc2b15909e4e5d
krupal4 <krupalvasani222@gmail.com>
2024-11-25 19:06:01 +0530

    remove comments improve UI

--- lib/linkedin_reactions_screen.dart
+++ lib/linkedin_reactions_screen.dart
@@ -1,21 +1,19 @@
 import 'dart:developer';
 import 'package:flutter/material.dart';
 
-class ReactionWidget extends StatefulWidget {
+class ReactionsButtonWidget extends StatefulWidget {
+  const ReactionsButtonWidget({super.key});
   @override
-  _ReactionWidgetState createState() => _ReactionWidgetState();
+  State<ReactionsButtonWidget> createState() => _ReactionsButtonWidgetState();
 }
 
-class _ReactionWidgetState extends State<ReactionWidget> {
-  // Hover state management
+class _ReactionsButtonWidgetState extends State<ReactionsButtonWidget> {
   final List<bool> _hoverStates = List.generate(5, (_) => false);
   int? _currentHoveredIndex;
   final _overlayController = OverlayPortalController();
 
-  // Emoji reactions
   final List<String> _reactions = ['👍', '❤️', '😂', '😲', '😢'];
 
-  // Font sizes
   final double normalSize = 40.0;
   final double shrunkSize = 30.0;
   final double expandedSize = 60.0;
@@ -27,7 +25,6 @@ class _ReactionWidgetState extends State<ReactionWidget> {
     _lastFontSizes = List.generate(5, (_) => normalSize);
   }
 
-  // Handle pointer enter
   void _onPointerEnter(int index) {
     if (_currentHoveredIndex != index) {
       setState(() {
@@ -35,24 +32,21 @@ class _ReactionWidgetState extends State<ReactionWidget> {
         _hoverStates[index] = true;
         _currentHoveredIndex = index;
       });
-      log('Entered reaction: ${_reactions[index]}');
+      // log('Entered reaction: ${_reactions[index]}');
     }
   }
 
-  // Handle pointer exit
   void _onPointerExit() {
     if (_currentHoveredIndex != null) {
-      log('Exited reaction: ${_reactions[_currentHoveredIndex!]}');
-      // Future.delayed(const Duration(microseconds: 500), () {
-        setState(() {
-          _resetHoverState();
-          _currentHoveredIndex = null;
-        });
-      // });
+      // log('Exited reaction: ${_reactions[_currentHoveredIndex!]}');
+
+      setState(() {
+        _resetHoverState();
+        _currentHoveredIndex = null;
+      });
     }
   }
 
-  // Reset all hover states
   void _resetHoverState() {
     for (int i = 0; i < _hoverStates.length; i++) {
       _hoverStates[i] = false;
@@ -65,51 +59,46 @@ class _ReactionWidgetState extends State<ReactionWidget> {
       onLongPress: _overlayController.toggle,
       child: OverlayPortal(
         controller: _overlayController,
-        child: const Icon(Icons.thumb_up_sharp),
+        child: const Icon(Icons.thumb_up_sharp,),
         overlayChildBuilder: (context) => Material(
           color: Colors.transparent,
-          child: SizedBox(
-            height: 100,
-            child: Listener(
-              onPointerMove: (event) {
-                // Determine which reaction is being hovered
-                for (int index = 0; index < _reactions.length; index++) {
-                  final box = context.findRenderObject() as RenderBox?;
-                  if (box != null) {
-                    // Calculate the local position of the pointer
-                    final localPosition = box.globalToLocal(event.position);
-                    final reactionWidth =
-                        (box.size.width - 16 * (_reactions.length - 1)) /
-                            _reactions.length;
-                    final reactionBounds = Rect.fromLTWH(
-                      index * (reactionWidth + 16),
-                      0,
-                      reactionWidth,
-                      box.size.height,
-                    );
-
-                    if (reactionBounds.contains(localPosition)) {
-                      _onPointerEnter(index);
-                      return;
-                    }
+          child: Listener(
+            onPointerMove: (event) {
+              for (int index = 0; index < _reactions.length; index++) {
+                final box = context.findRenderObject() as RenderBox?;
+                if (box != null) {
+                  final localPosition = box.globalToLocal(event.position);
+                  final reactionWidth =
+                      (box.size.width - 16 * (_reactions.length - 1)) /
+                          _reactions.length;
+                  final reactionBounds = Rect.fromLTWH(
+                    index * (reactionWidth + 16),
+                    0,
+                    reactionWidth,
+                    box.size.height,
+                  );
+          
+                  if (reactionBounds.contains(localPosition)) {
+                    _onPointerEnter(index);
+                    return;
                   }
                 }
-
-                // If no reaction is hovered, reset hover state
-                _onPointerExit();
-              },
-              onPointerUp: (event) {
-                // Reset hover state when the pointer is lifted
-                _onPointerExit();
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.transparent,
-                    borderRadius: BorderRadius.circular(16)),
+              }
+          
+              _onPointerExit();
+            },
+            onPointerUp: (event) {
+              _onPointerExit();
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              child: RedContainer(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: List.generate(_reactions.length, (index) {
-                    // Determine the font size based on hover state
                     double fontSize;
                     if (_hoverStates[index]) {
                       fontSize = expandedSize;
@@ -119,12 +108,11 @@ class _ReactionWidgetState extends State<ReactionWidget> {
                       fontSize = normalSize;
                     }
                     _lastFontSizes[index] = fontSize;
-
+          
                     return TweenAnimationBuilder(
                       tween: Tween(begin: _lastFontSizes[index], end: fontSize),
                       duration: const Duration(milliseconds: 200),
-                      // padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      builder:(context, size, _) => Text(
+                      builder: (context, size, _) => Text(
                         _reactions[index],
                         style: TextStyle(fontSize: size),
                       ),
@@ -136,6 +124,24 @@ class _ReactionWidgetState extends State<ReactionWidget> {
           ),
         ),
       ),
+    );
+  }
+}
+
+
+class RedContainer extends StatelessWidget {
+  const RedContainer({super.key, required this.child});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.red
+        )
+      ),
+      child: child,
     );
   }
 }
